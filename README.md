d267ba74aeaa1970a93b98a6150e0939ed960bf9
Your Name <you@example.com>
2024-09-12 13:02:34 +0300

    12.09.2024. Тихомиров/Филатов. В этот день мы выполнили Пз2, ЛЗ4-7. В них мы настраивали маршруты и создавали шаблоны для отдельных проектов

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# 1progectindgango
-1учебныйпроект
+# first-project-django
--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -2,7 +2,7 @@
 URL configuration for hello project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from firstapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index),
+    path('about/', views.about),
+    path('contact/', views.contact),
+    path('details/', views.details),
+    path('products/<int:productid>/', views.products),
+    path('users/', views.users),
+    path('personal/', views.personal)
 ]
+
