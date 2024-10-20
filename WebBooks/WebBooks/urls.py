aa610147250895a260c3d935bb2bf1e220a5c82a
Your Name <you@example.com>
2024-10-20 19:53:23 +0300

    ЛЗ12-13, ПЗ8-10. Формирование страниц. Тихомиров/Филатов

--- WebBooks/WebBooks/urls.py
+++ WebBooks/WebBooks/urls.py
@@ -2,7 +2,7 @@
 URL configuration for WebBooks project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,7 +16,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from catalog import views
+from django.urls import  re_path
+from django.urls import path, include
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+    re_path(r'^books/$', views.BookListView.as_view(), name='books'),
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    re_path(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
+    path('authors_add/', views.authors_add, name='authors_add'),
+
+]
+
+urlpatterns += [
+ path('accounts/', include('django.contrib.auth.urls')),
+]
+
+urlpatterns += [
+    re_path(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 ]
